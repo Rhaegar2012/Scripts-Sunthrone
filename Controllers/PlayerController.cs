7db4cb93daec57de1f1e45d158fb7143fbf26cba
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2023-04-09 14:30:49 -0700

    04092023-Update
    Modifications to read input in every frame trough update, no longer a Player Input component is needed. Player controller should handle all the movement including the interaction with the world , because is also an input call

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -6,17 +6,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRigidBody;
-    [SerializeField] private float forceMagnitude;
+    //Instance of player input action class, generate from input action asset before calling
+    private InputActions playerInputActions;
+    [SerializeField] private float speed;
+
     void Awake()
     {
         playerRigidBody= GetComponent<Rigidbody2D>();
+        playerInputActions= new InputActions();
+        
+        //Enable Player_Base action map
+        playerInputActions.Player_Base.Enable();
+        
     }
-
-    public  void Movement()
+    void Update()
     {
-        Debug.Log("Player movement");
-       
+        Vector2 movementDirection=playerInputActions.Player_Base.Movement.ReadValue<Vector2>();
+        playerRigidBody.velocity=new Vector2(movementDirection.x*speed,movementDirection.y*speed);
+
     }
+  
+
 
 
 
