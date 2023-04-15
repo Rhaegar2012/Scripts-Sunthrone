3d9ba68400661220ac8a52b624746936a415e58b
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2023-04-15 16:04:30 -0700

    04152023-Update
    -Implemented movement checks within PlayerMovement method
    -Added SetPlayerAnimation to set animation parameters within player animation controller blend tree

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -8,11 +8,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D playerRigidBody;
     //Instance of player input action class, generate from input action asset before calling
     private InputActions playerInputActions;
+    private Animator playerAnimator;
+    private Sprite playerSprite;
+    private bool isPlayerMoving;
     [SerializeField] private float speed;
 
     void Awake()
     {
         playerRigidBody= GetComponent<Rigidbody2D>();
+        playerAnimator=GetComponent<Animator>();
+        playerSprite=GetComponent<SpriteRenderer>().sprite;
         playerInputActions= new InputActions();
         //Enable Player_Base action map
         playerInputActions.Player_Base.Enable();
@@ -22,22 +27,66 @@ public class PlayerController : MonoBehaviour
     {
         
         Vector2 movementDirection=playerInputActions.Player_Base.Movement.ReadValue<Vector2>();
-        Vector3 offsetPosition=transform.position+new Vector3(movementDirection.x,movementDirection.y,0f);
-        if(LevelGrid.Instance.CheckPositionValid(offsetPosition))
+        if(movementDirection!=Vector2.zero)
         {
-            PlayerMovement(movementDirection);
+            isPlayerMoving=true;
         }
         else
         {
-            PlayerMovement(new Vector2(0f,0f));
+            isPlayerMoving=false;
         }
+        PlayerMovement(movementDirection);
+        
     }
 
     void PlayerMovement(Vector2 movementDirection)
     {
-       
+        //Allows movement only in one direction (no diagonal)
+        if(movementDirection.x!=0 && movementDirection.y!=0)
+        {
+            return;
+        }
+        //Checks that player is not outside grid limits
+        Vector3 offsetPosition=transform.position+new Vector3(movementDirection.x,movementDirection.y,0f);
+        if(!LevelGrid.Instance.CheckPositionValid(offsetPosition))
+        {
+            playerRigidBody.velocity= new Vector2 (0f,0f);
+            return;
+        }
         playerRigidBody.velocity= new Vector2(movementDirection.x*speed,movementDirection.y*speed);
+        //Animation parameter setup
+        if(movementDirection.x!=0)
+        {
+           
+            SetPlayerAnimation("moveX",movementDirection.x,isPlayerMoving);
+            SetPlayerAnimation("moveY",0f,isPlayerMoving);
+            transform.localScale=new Vector3(-movementDirection.x,1f,1f);
+        }
+        if(movementDirection.y!=0)
+        { 
+            SetPlayerAnimation("moveY",movementDirection.y,isPlayerMoving);
+            SetPlayerAnimation("moveX",0f,isPlayerMoving);
+        }
+        //Return to idle state
+        if(!isPlayerMoving)
+        {
+            SetPlayerAnimation("isMoving",0f,isPlayerMoving); 
+        }
+        
+    }
 
+    void SetPlayerAnimation(string animationParameter,float direction,bool isMoving)
+    {
+        if(isMoving)
+        {
+            playerAnimator.SetFloat(animationParameter,direction);
+            playerAnimator.SetBool("isMoving",isMoving);
+        }
+        else
+        {
+            playerAnimator.SetBool(animationParameter,isMoving);
+        }
+        
     }
   
 
