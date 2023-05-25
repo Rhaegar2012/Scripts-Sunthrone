63511fb0aff504add9e7d47d049ab368d63d425f
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2023-05-25 08:50:09 -0700

    05252023-Update

--- ScriptableObject/SO Level Management/SO_SceneState.cs
+++ ScriptableObject/SO Level Management/SO_SceneState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SO_SceneState : MonoBehaviour
+public class SO_SceneState : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
