ef52b36a479a7595695665683416df95f225e08e
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2023-06-01 08:57:29 -0700

    06012023-Update
    Added Class
    BattleSelectionMarker
    Modified Classes
    CommanderNPC

--- NPC/CommanderNPC.cs
+++ NPC/CommanderNPC.cs
@@ -1,9 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CommanderNPC : MonoBehaviour
 {
+    [SerializeField] private GameObject battleSelectionMenu;
+    [SerializeField] private List<GameObject> mapUIMarkers;
+    private SO_BattleInfo selectedBattle;
+    private List<SO_BattleInfo> battleInformation;
+    private List<Unit> availableUnits;
+
+    void Start()
+    {
+        battleInformation=GameManager.Instance.BattleInformation;
+        availableUnits=ArmyManager.Instance.ArmyUnitsList;
+        //Event subscription
+        PlayerController.Instance.onCommandMenuCalled+=PlayerController_OnCommandMenuCalled;
+    }
+
+    private void PlayerController_OnCommandMenuCalled(object sender,EventArgs empty)
+    {
+        battleSelectionMenu.SetActive(true);
+        DisplayBattleInformationMenu()
+    }
+
+    private void DisplayBattleInformationMenu()
+    {
+
+    }
+
+    private void DisplayBattleInformation(string battleName)
+    {
+        
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         PlayerController.Instance.ActiveCommanderNPC=this;
---
+++ UI/BattleSelectionMarker.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleSelectionMarker : MonoBehaviour
+{
+    [SerializeField] private Button battleSelectionButton;
+    [SerializeField] private Image selectionIconImage;
+    [SerializeField] private Sprite bronzeMedalSprite;
+    [SerializeField] private Sprite silverMedalSprite;
+    [SerializeField] private Sprite goldMedalSprite;
+
+    public void UpdateButtonIcon()
+    {
+        
+    }
+}
